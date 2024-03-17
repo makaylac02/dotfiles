fabc127b972b60bb09289dc330eee3461fae164b
makayla <makaylacarr74@gmail.com>
2024-03-17 00:20:40 -0400

    can check for apt

---
+++ install.sh
@@ -0,0 +1,19 @@
+
+# check if effective user id is 0 (root)
+if [[ "$(id -u)" -eq 0 ]]; then
+	echo "Script is running as root"
+	# check if apt is package manager
+
+	echo $(which apt)
+	if [[ -n "$(which apt)" ]]; then
+	echo "apt is installed exactly as specified."
+else
+	echo "apt is not installed at the specified location."
+	fi
+		# install packages with apt
+
+else 
+	echo "Script is not running as root, exiting..." 1>&2
+	exit 1
+
+fi
